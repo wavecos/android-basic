37d1ee65b32804b159e9dbe3b6e615829b006c67
Alfredo Arias <alfredo@xiobit.com>
2015-06-20 10:06:23 -0400

    first app hello world

---
+++ HelloWorld/app/src/main/java/com/xiobit/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xiobit.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by onix on 6/20/15.
+ */
+public class MainActivity extends Activity {
+
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.main_activity);
+    }
+
+}
