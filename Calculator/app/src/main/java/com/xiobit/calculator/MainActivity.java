ff2e02b60d521f24a3cba513ecdadce4c0d9c986
Alfredo Arias <alfredo@xiobit.com>
2015-06-23 15:22:12 -0400

    some basic functionallity

--- Calculator/app/src/main/java/com/xiobit/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/xiobit/calculator/MainActivity.java
@@ -1,17 +1,71 @@
 package com.xiobit.calculator;
 
+import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private static final String TAG = "Calculator";
+
+    TextView displayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "Initializing Calculator App...");
+
+        displayTextView = (TextView) findViewById(R.id.displayTextView);
+
+        findViewById(R.id.digit0Button).setOnClickListener(this);
+        findViewById(R.id.digit1Button).setOnClickListener(this);
+        findViewById(R.id.digit2Button).setOnClickListener(this);
+        findViewById(R.id.digit3Button).setOnClickListener(this);
+        findViewById(R.id.digit4Button).setOnClickListener(this);
+        findViewById(R.id.digit5Button).setOnClickListener(this);
+        findViewById(R.id.digit6Button).setOnClickListener(this);
+        findViewById(R.id.digit7Button).setOnClickListener(this);
+        findViewById(R.id.digit8Button).setOnClickListener(this);
+        findViewById(R.id.digit9Button).setOnClickListener(this);
+
+        // Clear Behavior
+        findViewById(R.id.clearButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayTextView.setText("0");
+            }
+        });
+
+        // Sin(x) function
+        findViewById(R.id.sinButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double displayValue = Double.valueOf( displayTextView.getText().toString() );
+                Double sinx = Math.sin(displayValue);
+                displayTextView.setText(sinx.toString());
+            }
+        });
+
+        // Cos(x) function
+        findViewById(R.id.cosButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double displayValue = Double.valueOf( displayTextView.getText().toString() );
+                Double cosx = Math.cos(displayValue);
+                displayTextView.setText(cosx.toString());
+            }
+        });
+
+
     }
 
     @Override
@@ -34,5 +88,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    // OnClickListener
+    @Override
+    public void onClick(View v) {
+        Button digitButton = (Button) v;
+        Log.i(TAG, "digit pressed : " + digitButton.getText());
+
+        displayTextView.setText( displayTextView.getText() + digitButton.getText().toString() );
+
+
+
     }
 }
